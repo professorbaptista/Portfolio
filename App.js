b2340e62dd6c82e04464bd7046cd12ed087240be
professorbaptista <148701836+professorbaptista@users.noreply.github.com>
2024-02-02 14:41:36 +0000

    Adicionei arquivo .js

---
+++ App.js
@@ -0,0 +1,9 @@
+// mudar = () =>{
+//     window.document.getElementById('anidire').style.backgroundColor ='#aca122'
+//     window.document.getElementById('anidire').style.color = '#000'
+// }
+
+// voltar = () =>{
+//     window.document.getElementById('anidire').style.backgroundColor ='#12b23c'
+//     window.document.getElementById('anidire').style.color = '#ffffff'
+// }
